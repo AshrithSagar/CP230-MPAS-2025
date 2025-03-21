d4f196f9d82ecae74f296868a7292df991473e68
Ashrith Sagar <ashrith9sagar@gmail.com>
2025-03-21 19:54:03 +0530

    A5

--- Assignments/Assignment-5/Codes/utils.py
+++ Assignments/Assignment-5/Codes/utils.py
@@ -5,10 +5,12 @@ Utility classes
 
 from typing import Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
 from numpy.typing import NDArray
 
-Coord = Tuple[float, float]
+Vec2 = Tuple[float, float]
 
 
 class TwoDSpace:
@@ -18,19 +20,34 @@ class TwoDSpace:
 
 class AttractiveField(TwoDSpace):
     def __init__(
-        self, dimensions: Tuple[float, float], goal: Coord, epsilon: float
+        self, dimensions: Tuple[float, float], goal: Vec2, epsilon: float
     ) -> None:
         super().__init__(dimensions)
         self.goal = goal
         self.epsilon = epsilon
 
-    def field(self, coord: Coord) -> NDArray:
+    def field(self, coord: Vec2) -> NDArray:
         return np.array([self.goal[0] - coord[0], self.goal[1] - coord[1]])
 
 
-class PointRobot:
-    def __init__(self, mass: float, position: Coord, velocity: float, vmax: float):
+class Body:
+    def __init__(self, mass: float, position: Vec2, velocity: Vec2):
         self.mass = mass
-        self.position: Coord = np.array(position)
-        self.velocity = velocity
+        self.position: Vec2 = np.array(position)
+        self.velocity: Vec2 = np.array(velocity)
+
+
+class Obstacle(Body):
+    def __init__(self, mass: float, position: Vec2, velocity: Vec2):
+        super().__init__(mass, position, velocity)
+
+
+class PointRobot(Body):
+    def __init__(self, mass: float, position: Vec2, velocity: Vec2, vmax: float):
+        super().__init__(mass, position, velocity)
         self.vmax = vmax  # Maximum horizontal velocity capability
+
+
+class Scene:
+    def __init__(self, time_step: float):
+        self.dt = time_step
