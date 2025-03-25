7ad157abe790e6da699233f26a0a40c1113688e8
Ashrith Sagar <ashrith9sagar@gmail.com>
2025-03-25 14:14:13 +0530

    A6

--- Assignments/Assignment-6/Codes/run.py
+++ Assignments/Assignment-6/Codes/run.py
@@ -7,17 +7,20 @@ from utils import *
 
 
 def main():
-    scene = Scene(time_step=0.01, sub_steps=1e2, scale=True)
+    scene = Scene(time_step=0.01, sub_steps=1e3, scale=True)
 
     robotA = Robot(radius=3)
     robotB = Robot(radius=4)
+    robotC = Robot(radius=5)
 
     voAB = VelocityObstacle(robotA, robotB)
+    voBC = VelocityObstacle(robotB, robotC)
+    voCA = VelocityObstacle(robotC, robotA)
 
-    scene.add_bodies([robotA, robotB])
-    scene.add_vos([voAB])
+    scene.add_bodies([robotA, robotB, robotC])
+    scene.add_vos([voAB, voBC, voCA])
 
-    scene.render(framerate=240, record=True)
+    scene.render(framerate=240, record=False)
 
 
 if __name__ == "__main__":
